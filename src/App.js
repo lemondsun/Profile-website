f9aef236ef7e94589b59e009f40c3e171a07e898
Jason Mullings <wjasonmullings@gmail.com>
2019-11-06 16:21:09 -0500

    setting up header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route } from "react-router-dom";
+import Body from "./components/Body";
+import Events from "./components/Events"
+class App extends React.Component {
+  state = {
+   
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+    
+  render() {
+    return (
+      <div>
+        <Header />
+        <Route exact path="/" render={() => <Body />} />
+        <Route path="/events" render={() => <Events />}/>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer(props) {
+  return (
+    <div id="footer">
+      <p>credits</p>
+    </div>
+  )
+}
+
+export default Footer;
